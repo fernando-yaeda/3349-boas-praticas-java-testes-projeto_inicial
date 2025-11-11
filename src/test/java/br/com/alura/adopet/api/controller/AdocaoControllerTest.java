acba2988cb78c3602a13d37cff4e877502ee9f58
fernando yaeda <fernandoyudi.yaeda@gmail.com>
2025-11-11 14:58:50 -0300

    test(adocao): add AdocaoContoller.solicitar unit tests

---
+++ src/test/java/br/com/alura/adopet/api/controller/AdocaoControllerTest.java
@@ -0,0 +1,67 @@
+package br.com.alura.adopet.api.controller;
+
+import br.com.alura.adopet.api.dto.SolicitacaoAdocaoDto;
+import br.com.alura.adopet.api.service.AdocaoService;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.json.AutoConfigureJsonTesters;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.json.JacksonTester;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@AutoConfigureJsonTesters
+class AdocaoControllerTest {
+
+    @Autowired
+    private MockMvc mvc;
+
+    @MockBean
+    private AdocaoService adocaoService;
+
+    @Autowired
+    private JacksonTester<SolicitacaoAdocaoDto> jsonDto;
+
+
+    @Test
+    void deveriaDevolverCodigo400ParaSolicitacaodeAdocaoComErros() throws Exception {
+        SolicitacaoAdocaoDto dto = new SolicitacaoAdocaoDto(
+                1L,
+                1L,
+                "motivo da adocao"
+        );
+
+        var response = mvc.perform(
+                post("/adocoes")
+                        .content(jsonDto.write(dto).getJson())
+                        .contentType(MediaType.APPLICATION_JSON)
+        ).andReturn().getResponse();
+
+        assertEquals(400, response.getStatus());
+    }
+
+    @Test
+    void deveriaDevolverCodigo200ParaSolicitacaodeAdocaoSemErros() throws Exception {
+        SolicitacaoAdocaoDto dto = new SolicitacaoAdocaoDto(
+                1L,
+                1L,
+                "motivo da adocao"
+        );
+
+        var response = mvc.perform(
+                post("/adocoes")
+                        .content(jsonDto.write(dto).getJson())
+                        .contentType(MediaType.APPLICATION_JSON)
+        ).andReturn().getResponse();
+
+        assertEquals(200, response.getStatus());
+    }
+}
